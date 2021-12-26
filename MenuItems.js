b126ebc42991f3df6aec57cecb1a7af7d46e9f06
Ucee <Ebilitenelson@gmail.com>
2021-12-26 05:54:09 +0100

    this is th navbar details

---
+++ MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+    
+    {
+        title: 'Sign in',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+]
+
+
+
+ 
